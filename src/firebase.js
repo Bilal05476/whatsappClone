1f7ba6010866d7c356cd00c02e35ca3e82a2baa3
Bilal Ahmed <55283639+Bilal05476@users.noreply.github.com>
2021-02-18 18:05:40 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBW4G_MbQpcCjZAfRgUT1ReAc-lJ56d8E4",
+  apiKey: "apiKey",
   authDomain: "mywhatsapp-clone-project.firebaseapp.com",
   projectId: "mywhatsapp-clone-project",
   storageBucket: "mywhatsapp-clone-project.appspot.com",
   messagingSenderId: "711010424423",
-  appId: "1:711010424423:web:ad48963bec3e7d19e8e6c2",
+  appId: "appId",
   measurementId: "G-ZMLF71GZCJ",
 });
 
